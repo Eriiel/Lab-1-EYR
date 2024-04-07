d8852c42b8dfdcb3e88be7f0dc6ff7cc31c079f5
Andres27dd <166265123+Andres27dd@users.noreply.github.com>
2024-04-07 18:43:04 -0500

    Update Tres_cadenas.cpp

--- Tres_cadenas.cpp
+++ Tres_cadenas.cpp
@@ -5,13 +5,13 @@ using namespace std;
 int main() {
     string c, CC;
     
-    for (int i = 1; i <= 3; ++i) {
-        cout << "Introduzca una cadena de caracteres: ";  
-        getline(cin, c);
-        CC = CC + c;
+    for (int i = 1; i <= 3; ++i) {                           /*Determina la cantidad de veces que el ciclo se repite que son 3*/
+        cout << "Introduzca una cadena de caracteres: ";     /*Permite digitar las cadenas y por cada vuelta del ciclo*/
+        getline(cin, c);                                     /*Al usar getline permite insertar e imprimir cadenas con tilde y ñ*/
+        CC = CC + c;                                         /*El acumulador permite unir las 3 cadenas que se insertar en el ciclo*/
     }
     
-    cout << "Las cadenas unificadas son: " << CC << endl;
+    cout << "Las cadenas unificadas son: " << CC << endl;    /*Imprime la cadena unificada*/
     
     return 0;
 }
