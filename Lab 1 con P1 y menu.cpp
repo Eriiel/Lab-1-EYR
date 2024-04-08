23d319963791179560593ce3a386a3827b8b8887
eeeamir <166254828+eeeamir@users.noreply.github.com>
2024-04-07 22:05:21 -0500

    Lab 1 con P1, P2, P4 y menu.cpp

--- Lab 1 con P1 y menu.cpp
+++ Lab 1 con P1 y menu.cpp
@@ -31,7 +31,7 @@ int main()
     
     for (int i = 1; i <= 3; ++i) {                           /*Determina la cantidad de veces que el ciclo se repite que son 3*/
         cout << "Introduzca una cadena de caracteres: ";     /*Permite digitar las cadenas y por cada vuelta del ciclo*/
-        getline(cin, c);                                     /*Al usar getline permite insertar e imprimir cadenas con tilde y ñ*/
+        getline(cin, c);                                     /*Al usar getline permite insertar e imprimir cadenas con tilde y Ã±*/
         CC = CC + c;                                         /*El acumulador permite unir las 3 cadenas que se insertar en el ciclo*/
     }
     
@@ -39,8 +39,44 @@ int main()
 		break;
 	}
 	
-	case 0: {
-		cout<<"A cerrado el programa\n";
+	case 4: {
+		string cad1;
+        int lon, lon2, i;
+        string subcad;
+            
+        cout << "Ingrese su cadena: ";
+        cin.ignore();
+        getline(cin, cad1);
+        lon = cad1.length();
+
+        if (lon > 1) {
+        cout << "Su cadena es la siguiente: " << cad1 << endl << "y su longitud es la siguiente: " << lon << endl;
+            } else {
+                cout << "Su cadena es muy corta, ingrese otra" << endl;
+                return 0;
+            }
+            
+        cout << "Ingrese la posicion inicial de su subcadena: ";
+            if (!(cin >> i)) {
+                cout << "La posicion inicial debe ser un numero en rango con la cadena" << endl;
+                return 0;
+            }
+            
+        cout << "Ingrese la longitud de su subcadena: ";
+        cin >> lon2;
+            
+            if (i >= 0 && i < lon) {
+                subcad = cad1.substr(i, lon2);
+                cout << "Su subcadena es: " << subcad << endl;
+                } else {
+                cout << "Su subcadena debe ser un numero en rango con la cadena" << endl;
+                return 0;
+                
+               }
+	      break;}
+        
+	  case 0:{
+		cout<<"Ha cerrado el programa\n";
 		break;
 	}
 	default: {
@@ -48,10 +84,8 @@ int main()
 		break;
 	}
 	
-    }
-	
+
 	system("pause");
     return 0;
-}
-
+}}
 
